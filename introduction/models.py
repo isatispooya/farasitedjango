34dc02e09a4b389269feb2daeee7fc99c26a0a0c
Mahya Abouei <mahyaaboueii@gmail.com>
2024-07-22 12:18:16 +0330

    .

--- introduction/models.py
+++ introduction/models.py
@@ -1,6 +1,27 @@
 from django.db import models
+from website.models import Domain
 
-class IntroBanner(models.Model):
-    title = models.CharField()
-    discription = models.CharField(max_length=100)
-    domain = models.CharField(max_length=64, unique=True)
+
+
+# cards for about investment page
+class Card (models.Model) :
+    Card = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.Card
+
+# all cards and title  for about investment page 
+class Introcard (models.Model) :
+    Title = models.CharField(max_length=1000)
+    Card = models.ManyToManyField(
+        Card ,
+        related_name='Intro_card',
+        blank=True,
+        help_text='Specific Card for introcard.',
+        verbose_name='introcard')
+                                   
+    Domain = models.ForeignKey(Domain ,to_field='domain' ,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.Title
+    
