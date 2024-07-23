98fba8d96611447b73cab6cd3fa319734e53c01d
Mahya Abouei <mahyaaboueii@gmail.com>
2024-07-23 09:06:50 +0330

    vision

--- vision/models.py
+++ vision/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 from django_summernote.fields import SummernoteTextField
+from website.models import Domain 
 
-
-class ContextDrop(models.model):
-    title = models.CharField(max_length=500)
-    summer = SummernoteTextField()
+class ContentDrop(models.Model):
+    Title = models.CharField(max_length=500)
+    Summer = SummernoteTextField()
 
 
     def __str__(self) :
-        return f'{self.title()}'
+        return f'{self.Title()}'
+    
 
+class TabVision (models.Model) :
+    Title = models.CharField(max_length=200)
+    Summer = SummernoteTextField()
+    Domain = models.ForeignKey (Domain , to_field='domain' , on_delete=models.CASCADE)
+    Contentdrop = models.ManyToManyField (    
+    ContentDrop,
+    related_name= 'content_drop', 
+    blank=True, help_text= 'Specific content for tab vision.', 
+    verbose_name = 'summernot for content' )
 
+    def __str__(self):
+        return f'{self.Title()}'
