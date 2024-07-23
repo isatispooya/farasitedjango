5b4234e0a0c5d3859e13fbbe87f67f45955408ae
rahele <rahelemomenyfar0113@gmail.com>
2024-07-23 08:56:41 +0330

    .

--- vision/serializers.py
+++ vision/serializers.py
@@ -1,1 +1,8 @@
 from rest_framework import serializers
+from .models import ContextDrop
+
+class ContextDropSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContextDrop
+        fields = '__all__'
+        
