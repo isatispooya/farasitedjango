2b567bf26049ae6360cd39ade9d203c046d3e91c
rahele <rahelemomenyfar0113@gmail.com>
2024-07-23 09:31:56 +0330

    .

--- vision/views.py
+++ vision/views.py
@@ -1,7 +1,6 @@
-
 from rest_framework import serializers
 from rest_framework import viewsets
 from rest_framework import response
 from . import models
-from . import serializer
+from . import serializers
 
