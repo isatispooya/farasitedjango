0e57ce745c532479689f367dbadbccf7c4c431b4
Mahya Abouei <mahyaaboueii@gmail.com>
2024-07-24 10:10:39 +0330

    bourse

--- bourse/models.py
+++ bourse/models.py
@@ -1,5 +1,12 @@
-
+from website.models import Domain
 from django.db import models
+
+class Card(models.Model):
+   Title = models.CharField(max_length=200)
+   Description = models.CharField(max_length=2000) 
+
+   def __str__(self) :
+        return f'{self.Title}'
 
 class Introduction(models.Model):
    Title = models.CharField(max_length=200)
@@ -21,4 +28,27 @@ class Content(models.Model):
    
    def __str__(self) :
         return f'{self.Name}'
+
+class Sections (models.Model):
+   Title = models.CharField(max_length=200)
+   Content = models.ManyToManyField(
+        Content ,
+        related_name='content',
+        blank=True,
+        help_text='Specific content for sections.',
+        verbose_name='content')
+   
+   Card = models.ManyToManyField(
+        Card ,
+        related_name='cards',
+        blank=True,
+        help_text='Specific cards for sections.',
+        verbose_name='card')
+   
+   Domain = models.ForeignKey(Domain, to_field='domain', on_delete=models.CASCADE)
+
+   def __str__(self) :
+        return f'{self.Title}'
+
+
 
