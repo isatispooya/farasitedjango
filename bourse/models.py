5a410b5a1ca18935f6cbdec68ae4a39750b58d33
Mahya Abouei <mahyaaboueii@gmail.com>
2024-07-24 10:03:08 +0330

    bourse

--- bourse/models.py
+++ bourse/models.py
@@ -1,2 +1,17 @@
 from django.db import models
+from website.models import Domain
 
+
+# cards
+class Card (models.Model) :
+    Title = models.CharField(max_length=150)
+    Description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.Title
+
+# main page for introductions of bourse buttons in isatispooya.com
+class Sections (models.Model) :
+    Title = models.CharField(max_length=300)
+    Domain = models.ForeignKey(Domain , to_field='domain' , on_delete=models.CASCADE)
+    
