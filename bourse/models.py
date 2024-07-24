3410c32def40768721f03e5d68404c4ba7345144
rahele <rahelemomenyfar0113@gmail.com>
2024-07-24 09:51:22 +0330

    .

--- bourse/models.py
+++ bourse/models.py
@@ -1,2 +1,17 @@
 from django.db import models
 
+class Introduction(models.Model):
+   Title = models.CharField(max_length=200)
+   Description = models.CharField(max_length=2000) 
+   Photo = models.ImageField(upload_to='static/images/')
+
+
+class Content(models.Model):
+   Name = models.CharField(max_length=200)
+   Introduction = models.ManyToManyField(
+        Introduction ,
+        related_name='i_introduction',
+        blank=True,
+        help_text='Specific name for introduction.',
+        verbose_name='introduction')
+
