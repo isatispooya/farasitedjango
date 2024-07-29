6b89e71e5b139f2da0f03f38bf999621e374e91c
Mahya Abouei <mahyaaboueii@gmail.com>
2024-07-29 08:11:34 +0330

    bourse

--- bourse/models.py
+++ bourse/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Card(models.Model):
    Title = models.CharField(max_length=200)
    Description = models.CharField(max_length=2000) 
+   photo  =models.ImageField(upload_to= 'static/images/', null=True , blank= True)
 
    def __str__(self) :
         return f'{self.Title}'
